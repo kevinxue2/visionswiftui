828fcd1060e275c879414b396dd4941aef76b447
kevinxue2 <kevin.xue27@gmail.com>
2024-07-07 13:33:25 -0400

    Initial Commit

---
+++ visionswiftui/visionswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionswiftuiApp.swift
+//  visionswiftui
+//
+//  Created by Kevin Xue on 2024-07-07.
+//
+
+import SwiftUI
+
+@main
+struct visionswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
